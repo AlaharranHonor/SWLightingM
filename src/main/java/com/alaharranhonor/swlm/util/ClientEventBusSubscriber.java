e06d41a2dd7766fda6ebfa744ae6a1cc90becc00
Magnus Jensen <magnushjensen.mail@gmail.com>
2022-01-08 15:17:55 +0100

    removed swlm_ in front of all registry names

--- src/main/java/com/alaharranhonor/swlm/util/ClientEventBusSubscriber.java
+++ src/main/java/com/alaharranhonor/swlm/util/ClientEventBusSubscriber.java
@@ -14,28 +14,28 @@ public class ClientEventBusSubscriber {
 
     @SubscribeEvent
     public static void onCLientSetup(FMLClientSetupEvent event) {
-        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_ACACIA_LEAVES.get(), RenderType.getCutout());
-        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_BIRCH_LEAVES.get(), RenderType.getCutout());
-        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_DARK_OAK_LEAVES.get(), RenderType.getCutout());
-        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_JUNGLE_LEAVES.get(), RenderType.getCutout());
-        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_OAK_LEAVES.get(), RenderType.getCutout());
-        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_SPRUCE_LEAVES.get(), RenderType.getCutout());
-        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_BLACK_STAINED_GLASS.get(), RenderType.getTranslucent());
-        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_BLUE_STAINED_GLASS.get(), RenderType.getTranslucent());
-        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_BROWN_STAINED_GLASS.get(), RenderType.getTranslucent());
-        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_CYAN_STAINED_GLASS.get(), RenderType.getTranslucent());
-        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_GRAY_STAINED_GLASS.get(), RenderType.getTranslucent());
-        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_GREEN_STAINED_GLASS.get(), RenderType.getTranslucent());
-        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_LIGHT_BLUE_STAINED_GLASS.get(), RenderType.getTranslucent());
-        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_LIGHT_GRAY_STAINED_GLASS.get(), RenderType.getTranslucent());
-        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_LIME_STAINED_GLASS.get(), RenderType.getTranslucent());
-        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_MAGENTA_STAINED_GLASS.get(), RenderType.getTranslucent());
-        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_ORANGE_STAINED_GLASS.get(), RenderType.getTranslucent());
-        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_PINK_STAINED_GLASS.get(), RenderType.getTranslucent());
-        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_PURPLE_STAINED_GLASS.get(), RenderType.getTranslucent());
-        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_RED_STAINED_GLASS.get(), RenderType.getTranslucent());
-        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_WHITE_STAINED_GLASS.get(), RenderType.getTranslucent());
-        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_YELLOW_STAINED_GLASS.get(), RenderType.getTranslucent());
-        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_GRASS_BLOCK.get(), RenderType.getCutoutMipped());
+        RenderTypeLookup.setRenderLayer(BlockInit.ACACIA_LEAVES.get(), RenderType.getCutout());
+        RenderTypeLookup.setRenderLayer(BlockInit.BIRCH_LEAVES.get(), RenderType.getCutout());
+        RenderTypeLookup.setRenderLayer(BlockInit.DARK_OAK_LEAVES.get(), RenderType.getCutout());
+        RenderTypeLookup.setRenderLayer(BlockInit.JUNGLE_LEAVES.get(), RenderType.getCutout());
+        RenderTypeLookup.setRenderLayer(BlockInit.OAK_LEAVES.get(), RenderType.getCutout());
+        RenderTypeLookup.setRenderLayer(BlockInit.SPRUCE_LEAVES.get(), RenderType.getCutout());
+        RenderTypeLookup.setRenderLayer(BlockInit.BLACK_STAINED_GLASS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(BlockInit.BLUE_STAINED_GLASS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(BlockInit.BROWN_STAINED_GLASS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(BlockInit.CYAN_STAINED_GLASS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(BlockInit.GRAY_STAINED_GLASS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(BlockInit.GREEN_STAINED_GLASS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(BlockInit.LIGHT_BLUE_STAINED_GLASS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(BlockInit.LIGHT_GRAY_STAINED_GLASS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(BlockInit.LIME_STAINED_GLASS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(BlockInit.MAGENTA_STAINED_GLASS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(BlockInit.ORANGE_STAINED_GLASS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(BlockInit.PINK_STAINED_GLASS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(BlockInit.PURPLE_STAINED_GLASS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(BlockInit.RED_STAINED_GLASS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(BlockInit.WHITE_STAINED_GLASS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(BlockInit.YELLOW_STAINED_GLASS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(BlockInit.GRASS_BLOCK.get(), RenderType.getCutoutMipped());
     }
 }
--- src/main/java/com/alaharranhonor/swlm/util/ForgeBusEventSubscriber.java
+++ src/main/java/com/alaharranhonor/swlm/util/ForgeBusEventSubscriber.java
@@ -2,10 +2,15 @@ package com.alaharranhonor.swlm.util;
 
 import com.alaharranhonor.swlm.SWLM;
 import com.alaharranhonor.swlm.world.gen.SWLMOreGen;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.biome.Biome;
+import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ForgeRegistries;
 
 @Mod.EventBusSubscriber(modid = SWLM.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class ForgeBusEventSubscriber {
@@ -19,6 +24,24 @@ public class ForgeBusEventSubscriber {
 			// Nether oregen
 		} else {
 			SWLMOreGen.generateOverworldOres(event);
+		}
+	}
+
+	@SubscribeEvent
+	public static void onBlockMissingMappings(RegistryEvent.MissingMappings<Block> event) {
+		for (RegistryEvent.MissingMappings.Mapping<Block> mapping : event.getAllMappings()) {
+			if (mapping.key.getNamespace().equalsIgnoreCase("swem")) {
+				mapping.remap(ForgeRegistries.BLOCKS.getValue(new ResourceLocation("swlm", mapping.key.getPath().substring(16))));
+			}
+		}
+	}
+
+	@SubscribeEvent
+	public static void onItemMissingMappings(RegistryEvent.MissingMappings<Item> event) {
+		for (RegistryEvent.MissingMappings.Mapping<Item> mapping : event.getAllMappings()) {
+			if (mapping.key.getNamespace().equalsIgnoreCase("swem")) {
+				mapping.remap(ForgeRegistries.ITEMS.getValue(new ResourceLocation("swlm", mapping.key.getPath().substring(16))));
+			}
 		}
 	}
 }
