676e778f8b33250ca78c054a141dabdb5975af9c
Magnus Jensen <magnushjensen.mail@gmail.com>
2022-02-26 02:17:38 +0100

    rebase

--- src/main/java/com/alaharranhonor/swlm/util/ClientEventBusSubscriber.java
+++ src/main/java/com/alaharranhonor/swlm/util/ClientEventBusSubscriber.java
@@ -14,6 +14,7 @@ public class ClientEventBusSubscriber {
 
     @SubscribeEvent
     public static void onCLientSetup(FMLClientSetupEvent event) {
+<<<<<<< HEAD
         RenderTypeLookup.setRenderLayer(BlockInit.ACACIA_LEAVES.get(), RenderType.cutout());
         RenderTypeLookup.setRenderLayer(BlockInit.BIRCH_LEAVES.get(), RenderType.cutout());
         RenderTypeLookup.setRenderLayer(BlockInit.DARK_OAK_LEAVES.get(), RenderType.cutout());
@@ -37,5 +38,30 @@ public class ClientEventBusSubscriber {
         RenderTypeLookup.setRenderLayer(BlockInit.WHITE_STAINED_GLASS.get(), RenderType.translucent());
         RenderTypeLookup.setRenderLayer(BlockInit.YELLOW_STAINED_GLASS.get(), RenderType.translucent());
         RenderTypeLookup.setRenderLayer(BlockInit.GRASS_BLOCK.get(), RenderType.cutoutMipped());
+=======
+        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_ACACIA_LEAVES.get(), RenderType.getCutout());
+        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_BIRCH_LEAVES.get(), RenderType.getCutout());
+        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_DARK_OAK_LEAVES.get(), RenderType.getCutout());
+        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_JUNGLE_LEAVES.get(), RenderType.getCutout());
+        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_OAK_LEAVES.get(), RenderType.getCutout());
+        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_SPRUCE_LEAVES.get(), RenderType.getCutout());
+        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_BLACK_STAINED_GLASS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_BLUE_STAINED_GLASS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_BROWN_STAINED_GLASS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_CYAN_STAINED_GLASS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_GRAY_STAINED_GLASS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_GREEN_STAINED_GLASS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_LIGHT_BLUE_STAINED_GLASS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_LIGHT_GRAY_STAINED_GLASS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_LIME_STAINED_GLASS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_MAGENTA_STAINED_GLASS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_ORANGE_STAINED_GLASS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_PINK_STAINED_GLASS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_PURPLE_STAINED_GLASS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_RED_STAINED_GLASS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_WHITE_STAINED_GLASS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_YELLOW_STAINED_GLASS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(BlockInit.SWLM_GRASS_BLOCK.get(), RenderType.getCutoutMipped());
+>>>>>>> 9b5e33d... Add more blocks
     }
 }
--- src/main/java/com/alaharranhonor/swlm/util/ModEventBusSubscriber.java
+++ src/main/java/com/alaharranhonor/swlm/util/ModEventBusSubscriber.java
@@ -6,7 +6,10 @@ import com.alaharranhonor.swlm.config.ConfigHolder;
 import com.alaharranhonor.swlm.util.init.BlockInit;
 import com.alaharranhonor.swlm.world.gen.OreGenUtils;
 import com.alaharranhonor.swlm.world.gen.SWLMOreGen;
+<<<<<<< HEAD
 import net.minecraft.block.Block;
+=======
+>>>>>>> 9b5e33d... Add more blocks
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.client.renderer.RenderType;
@@ -14,7 +17,10 @@ import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraft.client.renderer.color.BlockColors;
 import net.minecraft.client.renderer.color.ItemColors;
 import net.minecraft.item.BlockItem;
+<<<<<<< HEAD
 import net.minecraft.util.ResourceLocation;
+=======
+>>>>>>> 9b5e33d... Add more blocks
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.FoliageColors;
 import net.minecraft.world.GrassColors;
@@ -22,12 +28,18 @@ import net.minecraft.world.IBlockDisplayReader;
 import net.minecraft.world.biome.BiomeColors;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.ColorHandlerEvent;
+<<<<<<< HEAD
 import net.minecraftforge.event.RegistryEvent;
+=======
+>>>>>>> 9b5e33d... Add more blocks
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+<<<<<<< HEAD
 import net.minecraftforge.registries.ForgeRegistries;
+=======
+>>>>>>> 9b5e33d... Add more blocks
 
 @Mod.EventBusSubscriber(modid = SWLM.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModEventBusSubscriber {
@@ -51,6 +63,7 @@ public class ModEventBusSubscriber {
 		public static void RegisterBlockColors(ColorHandlerEvent.Block event) {
 			BlockColors colors = event.getBlockColors();
 			colors.register((state, reader, pos, color) -> {
+<<<<<<< HEAD
 				return reader != null && pos != null ? BiomeColors.getAverageFoliageColor(reader, pos) : FoliageColors.getDefaultColor();
 			}, BlockInit.ACACIA_LEAVES.get(), BlockInit.JUNGLE_LEAVES.get(), BlockInit.DARK_OAK_LEAVES.get(), BlockInit.OAK_LEAVES.get());
 			colors.register((state, reader, pos, color) -> {
@@ -62,15 +75,35 @@ public class ModEventBusSubscriber {
 			colors.register((state, reader, pos, color) -> {
 				return reader != null && pos != null ? BiomeColors.getAverageGrassColor(reader, pos) : GrassColors.get(0.5D, 1.0D);
 			}, BlockInit.GRASS_BLOCK.get());
+=======
+				return reader != null && pos != null ? BiomeColors.getFoliageColor(reader, pos) : FoliageColors.getDefault();
+			}, BlockInit.SWLM_ACACIA_LEAVES.get(), BlockInit.SWLM_JUNGLE_LEAVES.get(), BlockInit.SWLM_DARK_OAK_LEAVES.get(), BlockInit.SWLM_OAK_LEAVES.get());
+			colors.register((state, reader, pos, color) -> {
+				return FoliageColors.getSpruce();
+			}, BlockInit.SWLM_SPRUCE_LEAVES.get());
+			colors.register((state, reader, pos, color) -> {
+				return FoliageColors.getBirch();
+			}, BlockInit.SWLM_BIRCH_LEAVES.get());
+			colors.register((state, reader, pos, color) -> {
+				return reader != null && pos != null ? BiomeColors.getGrassColor(reader, pos) : GrassColors.get(0.5D, 1.0D);
+			}, BlockInit.SWLM_GRASS_BLOCK.get());
+>>>>>>> 9b5e33d... Add more blocks
 		}
 		@SubscribeEvent
 		public static void RegisterItemColors(ColorHandlerEvent.Item event) {
 			ItemColors colors = event.getItemColors();
 			colors.register((stack, color) -> {
+<<<<<<< HEAD
 				BlockState blockstate = ((BlockItem)stack.getItem()).getBlock().defaultBlockState();
 				return event.getBlockColors().getColor(blockstate, (IBlockDisplayReader)null, (BlockPos)null, color);
 			}, BlockInit.GRASS_BLOCK.get(), BlockInit.ACACIA_LEAVES.get(), BlockInit.JUNGLE_LEAVES.get(), BlockInit.DARK_OAK_LEAVES.get(), BlockInit.OAK_LEAVES.get(),
 					BlockInit.SPRUCE_LEAVES.get(), BlockInit.BIRCH_LEAVES.get());
+=======
+				BlockState blockstate = ((BlockItem)stack.getItem()).getBlock().getDefaultState();
+				return event.getBlockColors().getColor(blockstate, (IBlockDisplayReader)null, (BlockPos)null, color);
+			}, BlockInit.SWLM_GRASS_BLOCK.get(), BlockInit.SWLM_ACACIA_LEAVES.get(), BlockInit.SWLM_JUNGLE_LEAVES.get(), BlockInit.SWLM_DARK_OAK_LEAVES.get(), BlockInit.SWLM_OAK_LEAVES.get(),
+					BlockInit.SWLM_SPRUCE_LEAVES.get(), BlockInit.SWLM_BIRCH_LEAVES.get());
+>>>>>>> 9b5e33d... Add more blocks
 		}
 	}
 }
